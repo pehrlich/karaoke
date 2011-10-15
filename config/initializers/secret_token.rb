c69dbb31dbd9556db650b396d4bb9eb8b7b4b9cc
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2011-10-14 23:11:02 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karaoke::Application.config.secret_token = '09e5f2768ff608d2d6e4490e80353aab5432dc943ec6ef3dd7c19fb0d169c07e9a283d835426be868cfc8113e2e04ea0be807e2a4d904e40fed4cebd557392d9'
