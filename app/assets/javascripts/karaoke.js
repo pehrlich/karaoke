97142daacee12c4c6de46d763377fb7d41623bd4
Mike Kascel <mkascel@gmail.com>
2011-10-15 16:04:09 -0700

    new opentok js

--- app/assets/javascripts/karaoke.js
+++ app/assets/javascripts/karaoke.js
@@ -1,136 +1,32 @@
 $(document).ready(function() {
-  var API_KEY = '5933671l';
-  var TOKEN = 'moderator_token';
-  var recorderManager;
-  var recorder;
-  var player;
-  var archive;
-  var recordPane;
-  var myArchives = new Object();
+    var API_KEY = '5959221';
+    var TOKEN = 'moderator_token';
+    var recorderManager;
+    var recorder;
+    var player;
 
-  TB.setLogLevel(TB.DEBUG);
+    TB.setLogLevel(TB.DEBUG);
 
-  myArchives.archive1 = archive1;
-  myArchives.archive2 = archive2;
-
-  recorderManager = TB.initRecorderManager(API_KEY);
-
-  // video container div
-  var video1Container = document.createElement('div');
-  video1Container.setAttribute('id', 'video1Container');
-  // div that will be replaced by first video
-  var video1Div = document.createElement('div');
-  video1Div.setAttribute('id', 'video1');
-  $('#main').append(video1Container);
-  $('#' + video1Container.id).append(video1Div);
-
-  var video2Container = document.createElement('div');
-  video2Container.setAttribute('id', 'video2Container');
-  var video2Div = document.createElement('div');
-  video2Div.setAttribute('id', 'video2');
-  $('#main').append(video2Container);
-  $('#' + video2Container.id).append(video2Div);
-
-  // use myArchives.length instead?
-  if (!myArchives.archive1 && !myArchives.archive2) {
-    // new project
-    recordPane = 1;
-    recorder = recorderManager.displayRecorder(TOKEN, 'video1');
-    recorder.addEventListener('archiveSaved', archiveSavedHandler);
-  }
-  else if (myArchives.archive1 && !myArchives.archive2) {
-    // One existing archive
-    recordPane = 2;
-    player = recorderManager.displayPlayer(archive1, TOKEN, 'video1');
-    recorder = recorderManager.displayRecorder(TOKEN, 'video2');
-    recorder.addEventListener('archiveSaved', archiveSavedHandler);
-  }
-  else if (myArchives.archive1 && myArchives.archive2) {
-    player = recorderManager.displayPlayer(archive1, TOKEN, 'video1');
-    player2 = recorderManager.displayPlayer(archive2, TOKEN, 'video2');
-    player.addEventListener('playbackStarted', playbackStartedHandler);
-  }
-  else {
-    alert('Problem with archive logic!');
-  }
-
-  TB.addEventListener("exception", exceptionHandler);
-
-  function exceptionHandler(event) {
-    alert("Exception:" + event.title + ". " + event.message);
-  }
-
-  function archiveSavedHandler(event) {
-    archiveId = event.archives[0].archiveId;
-
-    //switch this to case statment
-    // determine which archive ID and DOM element to set by which pane was recorded from
-    if (recordPane == 1) {
-      myArchives.archive1 = archiveId;
-      elem = 'video1'
+    recorderManager = TB.initRecorderManager(API_KEY);
+  
+    function exceptionHandler(event) {
+        alert("Exception:" + event.title + ". " + event.message); 
     }
-    if (recordPane == 2) {
-      myArchives.archive2 = archiveId;
-      elem = 'video2'
+  
+    function archiveSavedHandler(event) {
+        var archiveId = event.archives[0].archiveId;
+        console.log(archiveId);
     }
 
-    saveArchive(projectId, archiveId, elem);
-  }
+    function showRecorder() {
+        var properties = { width: '800', height: '450' };
+        recorder = recorderManager.displayRecorder(TOKEN, 'opentok_recorder', properties);
+    }
 
-  function saveArchive(projectId, archiveId, elem) {
-    // Save archiveID to server
-    $.ajax({
-      url: '/saveArchiveData',
-      type: 'POST',
-//		    dataType: 'json',
-      data: { 'myArchives': myArchives, 'projectId': projectId },
-      error: function(data) {
-        alert('error');
-      },
-      success: function(data) {
-        alert('success!');
-        // remove recorder, add temp video div for player to appropriate container
-        if (elem == 'video1') {
-          recorderManager.removeRecorder(recorder);
-
-          var video1Div = document.createElement('div');
-          video1Div.setAttribute('id', 'video1');
-          $('#main').append(video1Container);
-          $('#' + video1Container.id).append(video1Div);
-
-          myArchives.archive1 = archiveId;
-          recordPane = 2;
-          player = recorderManager.displayPlayer(archiveId, TOKEN, elem);
-          recorder = recorderManager.displayRecorder(TOKEN, 'video2');
-          recorder.addEventListener('archiveSaved', archiveSavedHandler);
-        }
-        if (elem == 'video2') {
-          recorderManager.removeRecorder(recorder);
-
-          var video2Div = document.createElement('div');
-          video2Div.setAttribute('id', 'video2');
-          $('#main').append(video2Container);
-          $('#' + video2Container.id).append(video2Div);
-
-          myArchives.archive2 = archiveId;
-          player2 = recorderManager.displayPlayer(archiveId, TOKEN, elem);
-        }
-      }
-    });
-  }
+    function showPlayer(archiveId) {
+        var properties = { width: '400', height: '225' };
+        player = recorderManager.displayPlayer(TOKEN, 'opentok_player', properties);
+    }
+    
 });
-
-function playbackStartedHandler(event) {
-  player2.startPlayback();
-}
-
-
-
-
-
-
-
-
-
-
 
